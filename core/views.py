c1ea9f0de6a4ddb76d9f3d0bf0b7763c20fe6614
EGAMAGZ <gazapito1@hotmail.com>
2020-10-19 18:46:26 -0500

    Created view and html for team members

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from core.models import TeamMember
 from unities.models import Unity
 from django.shortcuts import render
 from django.views.generic.list import ListView
@@ -5,3 +6,7 @@ from django.views.generic.list import ListView
 # Create your views here.
 class UnityList(ListView):
     model=Unity
+
+class TeamMembersList(ListView):
+    model=TeamMember
+    template_name = 'core/team_members_list.html'
