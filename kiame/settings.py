2dd33d8d0b01d2b994daeca1591ac52b159e26bd
EGAMAGZ <gazapito1@hotmail.com>
2020-11-03 15:52:56 -0600

    Revert "ALL HOSTS & DEBUG=False"
    This reverts commit 85708faaa28db1a39d269a806f41c70a155bbe42.

--- kiame/settings.py
+++ kiame/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = ')=x3_*a7fuq76@6-2nnb8n9#fy@weqo4r!eq$!evh%px*hsx_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["gazap.pythonanywhere.com"]
 
 
 # Application definition
