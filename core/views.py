3d77ab66749622dffe1aaa50d443d39c40b2862f
EGAMAGZ <gazapito1@hotmail.com>
2020-11-19 20:26:43 -0600

    Error handler

--- core/views.py
+++ core/views.py
@@ -1,11 +1,14 @@
+from django.http.response import HttpResponse
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
+from django.shortcuts import render
 
 from core.models import TeamMember
 from util.mixins import VisitContextMixin
 
 
 # Create your views here.
+# https://medium.com/@MicroPyramid/handling-custom-error-pages-404-500-in-django-ff1f9e0cf2b5
 class TeamMembersList(VisitContextMixin,ListView):
     model = TeamMember
     template_name = 'core/team_members_list.html'
@@ -13,3 +16,9 @@ class TeamMembersList(VisitContextMixin,ListView):
 
 class Home(VisitContextMixin,TemplateView):
     template_name = 'core/home.html'
+
+def handler404(request, exception) -> HttpResponse:
+    return render(request,'core/404.html', status=404)
+
+def handler500(request) -> HttpResponse:
+    return render(request, 'core/500.html', status=500)
