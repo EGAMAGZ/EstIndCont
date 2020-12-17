3f5e9a45a8a5006448f8b0890738acb98e941b17
EGAMAGZ <gazapito1@hotmail.com>
2020-12-16 22:03:46 -0600

    Fixed error with handler404, missing menu elements

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+from typing import Dict, Iterable, List
+
 from django.http.response import HttpResponse
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
@@ -6,7 +8,7 @@ from django.db.models import QuerySet
 
 from core.models import TeamMember
 from util.mixins import VisitContextMixin
-from unities.models import UnityContent
+from unities.models import UnityContent, Unity
 
 # Create your views here.
 # https://medium.com/@MicroPyramid/handling-custom-error-pages-404-500-in-django-ff1f9e0cf2b5
@@ -23,7 +25,33 @@ class Home(VisitContextMixin, ListView):
 
 
 def handler404(request, exception) -> HttpResponse:
-    return render(request,'core/404.html', status=404)
+    try:
+        if not request.session['page_visited_session']:
+            request.session['page_visited_session'] = True
+    except KeyError:
+        request.session['page_visited_session'] = False
+
+    return render(request,'core/404.html', {
+        'page_visited': request.session['page_visited_session'],
+        'menu_elements': get_info_menu()
+        }, status=404)
 
 def handler500(request) -> HttpResponse:
     return render(request, 'core/500.html', status=500)
+
+def get_info_menu(self) -> Iterable[Dict[str,str]]:
+    menu_info:List[Dict[str,str]] = []
+    for unity in Unity.objects.all():
+        list_content_info = []
+        for content in UnityContent.objects.filter(unity=unity):
+            list_content_info.append({
+                'url': content.get_absolute_url(),
+                'title': content.title
+            })
+        menu_info.append({
+            'unity_number': unity.number,
+            'slug': unity.slug,
+            'content': list_content_info,
+            'name': unity.name
+        })
+    return menu_info
